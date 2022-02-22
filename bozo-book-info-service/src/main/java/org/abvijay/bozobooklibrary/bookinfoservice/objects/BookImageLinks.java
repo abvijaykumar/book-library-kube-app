47e6ff85dbbb7082911c54732507d7bcc7e154be
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2022-02-22 14:53:33 +0530

    Initial version

---
+++ bozo-book-info-service/src/main/java/org/abvijay/bozobooklibrary/bookinfoservice/objects/BookImageLinks.java
@@ -0,0 +1,59 @@
+package org.abvijay.bozobooklibrary.bookinfoservice.objects;
+
+public class BookImageLinks {
+    String smallThumbnail;
+    String thumbnail;
+    String small;
+    String medium;
+    String large;
+    String extraLarge;
+
+    public String getSmall() {
+        return this.small;
+    }
+
+    public void setSmall(String small) {
+        this.small = small;
+    }
+
+    public String getMedium() {
+        return this.medium;
+    }
+
+    public void setMedium(String medium) {
+        this.medium = medium;
+    }
+
+    public String getLarge() {
+        return this.large;
+    }
+
+    public void setLarge(String large) {
+        this.large = large;
+    }
+
+    public String getExtraLarge() {
+        return this.extraLarge;
+    }
+
+    public void setExtraLarge(String extraLarge) {
+        this.extraLarge = extraLarge;
+    }    
+
+    public String getSmallThumbnail() {
+        return this.smallThumbnail;
+    }
+
+    public void setSmallThumbnail(String smallThumbnail) {
+        this.smallThumbnail = smallThumbnail;
+    }
+
+    public String getThumbnail() {
+        return this.thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+}
