729f27ffe10f3c5757cdcdfa50d67529c8523f0f
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2022-09-08 17:24:20 +0530

    fixed bugs

--- book-library-service/build-native-image.sh
+++ book-library-service/build-native-image.sh
@@ -1,6 +1,6 @@
 export GRAALVM_HOME=$HOME/Downloads/graalvm-ce-java11-22.2.0/Contents/Home/
-export JAVA_HOME=${GRAALVM_HOME}
-export PATH=${GRAALVM_HOME}/bin:$PATH  
+export JAVA_HOME=$GRAALVM_HOME
+export PATH=$GRAALVM_HOME/bin:$PATH  
 ./mvnw package -Dnative -e
 
 docker build -f Dockerfile.native -t bozo-book-library-service  .
