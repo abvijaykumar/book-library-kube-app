218f90ff2b33b61f77c4b3df50c548aac0211c46
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2022-09-09 12:33:06 +0530

    fixed appliction.properties

---
+++ book-library-service/src/main/java/org/abvijay/bozobooklibrary/booklibraryservice/BookLibraryDataSourceCredentialProvider.java
@@ -0,0 +1,24 @@
+package org.abvijay.bozobooklibrary.booklibraryservice;
+
+import io.quarkus.arc.Unremovable;
+import io.quarkus.credentials.CredentialsProvider;
+import java.util.HashMap;
+import java.util.Map;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+@ApplicationScoped
+@Unremovable
+@Named("booklibrary-db-credentials-provider")
+public class BookLibraryDataSourceCredentialProvider  implements CredentialsProvider {
+    
+	@Override
+	public Map<String, String> getCredentials(String credentialsProviderName) {
+
+		Map<String, String> properties = new HashMap<>();
+		properties.put(USER_PROPERTY_NAME, "dbUser");
+		properties.put(PASSWORD_PROPERTY_NAME, "dbPassword");
+		return properties;
+	}
+
+}
