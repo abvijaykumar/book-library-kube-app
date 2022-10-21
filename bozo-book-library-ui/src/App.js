4c77b15d0506b9a718b96ac22cc7be1ca6e22d39
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2022-10-21 12:06:58 +0530

    Fixed the build

--- bozo-book-library-ui/src/App.js
+++ bozo-book-library-ui/src/App.js
@@ -6,22 +6,6 @@ import './components/Book-Catalogue.css';
 import Home from './components/Home';
 import {CurrentUserContext} from './components/CurrentUserContext';
 
-const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin');
-const options = {
-  headers: {
-    'my-header': 'header-value',
-  },
-  url: 'your-zipkin-url',
-  // optional interceptor
-  getExportRequestHeaders: () => {
-    return {
-      'my-header': 'header-value',
-    }
-  }
-}
-const exporter = new ZipkinExporter(options);
-tracer.addSpanProcessor(new BatchSpanProcessor(exporter));
-
 const App = () => {
   const [user, setUser] = useState(null);
   const value = useMemo(() => ({ user, setUser }), [user, setUser]);
