b3ba6e40b214a34719bea05af5e332e3b01b6a27
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2022-10-25 19:06:55 +0530

    Update fire.js

--- bozo-book-library-ui/src/components/fire.js
+++ bozo-book-library-ui/src/components/fire.js
@@ -3,13 +3,13 @@ import firebase from 'firebase';
 // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
-    apiKey: "AIzaSyD9EF4m00iZYVTrDIj0Z0EYbiAlYZ1Biaw",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: "bozo-book-library.firebaseapp.com",
     projectId: "bozo-book-library",
     storageBucket: "bozo-book-library.appspot.com",
-    messagingSenderId: "330987868039",
-    appId: "1:330987868039:web:ee290d7833d8f46b780d22",
-    measurementId: "G-7X34ZG7VCE"
+    messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID
   };
   // Initialize Firebase
   const fire = firebase.initializeApp(firebaseConfig);
