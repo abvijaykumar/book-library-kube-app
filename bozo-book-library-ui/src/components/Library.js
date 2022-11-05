ac0f5dd1291d310c7b8ff6d82ace7335061fa866
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2022-11-05 09:51:35 +0530

    Update Library.js

--- bozo-book-library-ui/src/components/Library.js
+++ bozo-book-library-ui/src/components/Library.js
@@ -19,8 +19,9 @@ const Library = (props) => {
         getLibraryBooks();
     }, [currentPage, searchQuery]);
 
-    const BOOK_INFO_SERVICE_URL = process.env.REACT_APP_BOOK_INFO_SERVICE_URL
-    const BOOK_LIB_SERVICE_URL= process.env.REACT_APP_BOOK_LIB_SERVICE_URL + '/booklib'
+    //const BOOK_INFO_SERVICE_URL = process.env.REACT_APP_BOOK_INFO_SERVICE_URL
+    const BOOK_LIB_SERVICE_URL = "https://" + window.location.hostname + "/booklib";
+    //const BOOK_LIB_SERVICE_URL= process.env.REACT_APP_BOOK_LIB_SERVICE_URL + '/booklib'
 
     const getBooks = (bookids) => {
         var bookidsJson = JSON.stringify(bookids);
