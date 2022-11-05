33a776da2442bc592fb04eb5090e83cb8427aa99
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2022-11-05 09:12:14 +0530

    Update App.js

--- bozo-book-library-ui/src/App.js
+++ bozo-book-library-ui/src/App.js
@@ -13,11 +13,13 @@ import {CurrentUserContext} from './components/CurrentUserContext';
 
 const { Resource } = require('@opentelemetry/resources');
 
+const REACT_APP_OTEL_URL = process.env.REACT_APP_OTEL_URL;
+
 const serviceName = "bozo-book-ui";
 const resource = new Resource({ "service.name": serviceName });
 const provider = new WebTracerProvider({ resource });
 const collector = new CollectorTraceExporter({
-  url: "http://opentelemetry-opentelemetry-collector.opentelemetry.svc.cluster.local:4318/v1/traces",
+  url: REACT_APP_OTEL_URL,
 });
 
 const App = () => {
