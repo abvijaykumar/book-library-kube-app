92070f80086a9314313dc94a270a6feb13b27c21
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2022-11-06 10:09:08 +0530

    Implemented OTEL

--- bozo-book-library-ui/src/App.js
+++ bozo-book-library-ui/src/App.js
@@ -1,26 +1,14 @@
 import React, { useState, useMemo } from 'react';
 
-import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
-import { CollectorTraceExporter } from '@opentelemetry/exporter-collector';
 
 
 import './App.css';
 import './components/Book-Catalogue.css';
 
 
-import Home from './components/Home';
-import {CurrentUserContext} from './components/CurrentUserContext';
 
-const { Resource } = require('@opentelemetry/resources');
 
-const REACT_APP_OTEL_URL = process.env.REACT_APP_OTEL_URL;
 
-const serviceName = "bozo-book-ui";
-const resource = new Resource({ "service.name": serviceName });
-const provider = new WebTracerProvider({ resource });
-const collector = new CollectorTraceExporter({
-  url: REACT_APP_OTEL_URL,
-});
 
 const App = () => {
   const [user, setUser] = useState(null);
--- bozo-book-library-ui/src/index.js
+++ bozo-book-library-ui/src/index.js
@@ -4,6 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
+import { CollectorTraceExporter } from '@opentelemetry/exporter-collector';
+import Home from './components/Home';
+import {CurrentUserContext} from './components/CurrentUserContext';
+const { Resource } = require('@opentelemetry/resources');
+
+const REACT_APP_OTEL_URL = process.env.REACT_APP_OTEL_URL;
+
+const serviceName = "bozo-book-ui";
+const resource = new Resource({ "service.name": serviceName });
+const provider = new WebTracerProvider({ resource });
+const collector = new CollectorTraceExporter({
+  url: REACT_APP_OTEL_URL,
+});
+
+
 ReactDOM.render(
     <App />,
   document.getElementById('root')
@@ -12,4 +28,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
