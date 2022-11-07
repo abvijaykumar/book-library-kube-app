3c1b9430d1a4583aa5991491f82c427b479c64cb
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2022-11-07 12:36:40 +0530

    Update App.js

--- bozo-book-library-ui/src/App.js
+++ bozo-book-library-ui/src/App.js
@@ -6,12 +6,9 @@ import './components/Book-Catalogue.css';
 import Home from './components/Home';
 import {CurrentUserContext} from './components/CurrentUserContext';
 
-import { usePerformanceMark, Stage } from '@cabify/prom-react';
-
 const App = () => {
   const [user, setUser] = useState(null);
   const value = useMemo(() => ({ user, setUser }), [user, setUser]);
-  usePerformanceMark(isLoading ? Stage.Usable : Stage.Complete, 'bozo-book-library-ui');
 
   return (
     <CurrentUserContext.Provider value={value}>
