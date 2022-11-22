e872dac083b1edf2772cf6d880b85faac8615f78
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2022-11-22 17:46:15 +0530

    fixed booinfo

--- bozo-book-info-service/src/main/java/org/abvijay/bozobooklibrary/bookinfoservice/BookInfoResource.java
+++ bozo-book-info-service/src/main/java/org/abvijay/bozobooklibrary/bookinfoservice/BookInfoResource.java
@@ -3,9 +3,7 @@ package org.abvijay.bozobooklibrary.bookinfoservice;
 import io.vertx.core.cli.annotations.Description;
 import java.util.List;
 import javax.inject.Inject;
-import javax.ws.rs.Path;
 import org.abvijay.bozobooklibrary.bookinfoservice.objects.BookInfoSearchResponse;
-
 import org.eclipse.microprofile.graphql.GraphQLApi;
 import org.eclipse.microprofile.graphql.Query;
 
